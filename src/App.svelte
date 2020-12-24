3bfda2a1cafe73775efd35973ff31747f6683166
Drache93 <drache9335@gmail.com>
2020-12-24 15:43:11 +0000

    Use tailiwind

--- src/App.svelte
+++ src/App.svelte
@@ -2,25 +2,12 @@
 	export let name: string;
 </script>
 
-<main>
-	<h1 class="text-red-500">Hello {name}!</h1>
+<main class="text-center p-4 max-w-xl my-0 mx-auto">
+	<h1 class="text-red-500 uppercase text-6xl font-thin">Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
